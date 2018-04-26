bb9588976381afa0276e2f9aac40344ee48aa33e
Salome Lambermont <salome.lambermont@gmail.com>
2018-04-26 09:48:23 +0100

    Test station class

--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -1,11 +1,14 @@
 require 'station'
 
 describe Station do
+  let(:a_station) { Station.new(:bank, 5) }
 
-  let(:station) { double :station }
   describe '#initalize' do
-    it 'should create a name' do
-      expect
+    it 'should assign a name to the station' do
+      expect(a_station.name).to eq :bank
+    end
+    it 'should assign a zone to the station' do
+      expect(a_station.zone).to eq 5
     end
   end
 end
